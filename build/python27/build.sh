e14bfc25247f4399d1d1a52c4ae78a3cfbf85396
Andy Fiddaman <omnios@citrus-it.co.uk>
2019-03-04 12:54:43 +0000

    python2: update to 2.7.16

--- build/python27/build.sh
+++ build/python27/build.sh
@@ -21,14 +21,13 @@
 # CDDL HEADER END }}}
 #
 # Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
-# Use is subject to license terms.
+# Copyright 2019 OmniOS Community Edition (OmniOSce) Association.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=Python
-VER=2.7.15
+VER=2.7.16
 PKG=runtime/python-27
 SUMMARY="$PROG ${VER%.*}"
 DESC="$SUMMARY"
