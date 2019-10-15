b6d2dbb76615c7cb2b21861239310d8582b46a8b
Andy Fiddaman <omnios@citrus-it.co.uk>
2019-10-15 09:16:39 +0000

    sudo: update to 1.8.28

--- build/sudo/build.sh
+++ build/sudo/build.sh
@@ -27,7 +27,7 @@
 . ../../lib/functions.sh
 
 PROG=sudo
-VER=1.8.27
+VER=1.8.28
 PKG=security/sudo
 SUMMARY="Authority delegation tool"
 DESC="Provide limited super-user privileges to specific users"
